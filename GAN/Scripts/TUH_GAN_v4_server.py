b5c0098983735b7fbf07fc26ecfdf4a0a22359d5
williamtheodor <wls@braincapture.dk>
2021-06-07 10:43:58 +0200

    StratifiedGroupKFold

--- GAN/Scripts/TUH_GAN_v4_server.py
+++ GAN/Scripts/TUH_GAN_v4_server.py
@@ -1,4 +1,5 @@
 import argparse
+from collections import defaultdict, Counter
 
 from matplotlib import pyplot
 from numpy import expand_dims, vstack
@@ -6,7 +7,6 @@ from numpy import ones
 from numpy import zeros
 from numpy.random import rand, randn
 from numpy.random import randint
-# from keras.datasets.mnist import load_data
 from keras.optimizers import Adam
 from keras.models import Sequential
 from keras.layers import Dense
@@ -16,12 +16,17 @@ from keras.layers import Dropout
 from keras.layers import LeakyReLU
 
 import numpy as np
-from sklearn.utils import resample
-from tensorflow.python.keras.layers import Reshape, Conv2DTranspose
-from tensorflow.python.keras.models import load_model
-
+from random import random
 from os.path import exists
 from os import makedirs
+
+from sklearn.utils import check_random_state
+from tensorflow.python.keras.layers import Reshape, Conv2DTranspose
+
+
+seed = 55784899
+np.random.seed(seed)
+random.seed(seed)
 
 
 parser = argparse.ArgumentParser()
@@ -34,6 +39,76 @@ artifact = args.artifact
 n_epochs = args.epochs
 
 outdir = 'generator_models'
+
+
+class StratifiedGroupKFold():
+
+    def __init__(self, k=10, n_repeats=1, seed=None):
+        self.k = k  # Number of folds
+        self.n_repeats = n_repeats  # Number of repeats
+        self.seed = seed  # Random seed for reproducibility
+
+    # Implementation inspired by Kaggle
+    # https://www.kaggle.com/jakubwasikowski/stratified-group-k-fold-cross-validation
+    def split(self, X, y=None, groups=None):
+        random_state = check_random_state(self.seed)  # Random state generator
+
+        # Repeat k-fold n_splits time with unique folds
+        for repeat in range(self.n_repeats):
+            labels_num = len(np.unique(y))  # Number of labels
+
+            # Calculate the label distribution for each group
+            y_counts_per_group = defaultdict(lambda: np.zeros(labels_num, dtype=int))
+            y_distr = Counter()
+
+            for label, g in zip(y, groups):
+                y_counts_per_group[g][label] += 1
+                y_distr[label] += 1
+
+            fold_counts = defaultdict(lambda: np.zeros(labels_num, dtype=int))
+            groups_per_fold = defaultdict(set)
+
+            # Shuffle the groups
+            label_counts = list(y_counts_per_group.items())
+            random_state.shuffle(label_counts)
+
+            '''
+            For each group and its label distribution add the group to the
+            fold that would cause the least standard deviation from the
+            original distribution.
+            '''
+
+            for g, label_counts in label_counts:
+                best_fold = None
+                min_eval = None
+                for fold in range(self.k):
+                    fold_counts[fold] += label_counts
+                    std_per_label = []
+                    for l in range(labels_num):
+                        label_std = np.std([fold_counts[i][l] / y_distr[l] for i in range(self.k)])
+                        std_per_label.append(label_std)
+
+                    fold_counts[fold] -= label_counts
+
+                    fold_eval = np.mean(std_per_label)
+
+                    if min_eval == None or fold_eval < min_eval:
+                        min_eval = fold_eval
+                        best_fold = fold
+
+                fold_counts[best_fold] += label_counts
+                groups_per_fold[best_fold].add(g)
+
+            all_groups = np.unique(groups)  # Get all unique groups
+            for fold in range(self.k):
+                train_groups = np.setdiff1d(all_groups, list(groups_per_fold[fold]))
+                test_groups = list(groups_per_fold[fold])
+
+                train_indices = np.where(np.isin(groups, list(train_groups)))[0]
+                test_indices = np.where(np.isin(groups, list(test_groups)))[0]
+
+                # Yields the indices as they are needed
+                yield repeat, fold, train_indices, test_indices
 
 
 def load_data():
@@ -58,15 +133,13 @@ def load_data():
 
     groups = groups[y_buf != 5]
 
-    idxs = resample(np.unique(groups), replace=False, n_samples=10, random_state=1928)
+    _, _, train_idx, test_idx = list(StratifiedGroupKFold(k=5, n_repeats=1, seed=55784899).split(X, y, groups))[0]
 
-    trainX = X[~np.isin(groups, idxs)]
-    trainy = y[~np.isin(groups, idxs)]
-
-    testX = X[np.isin(groups, idxs)]
-    testy = y[np.isin(groups, idxs)]
+    trainX, trainy = X[train_idx], y[train_idx]
+    testX, testy = X[test_idx], y[test_idx]
 
     return (trainX, trainy), (testX, testy)
+
 
 
 # define the standalone discriminator model
