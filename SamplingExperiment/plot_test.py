d48d903098bc6ef3e9c5e5547e0233b7c0548671
s194260 <s194260@student.dtu.dk>
2021-06-09 09:22:18 +0200

    Data analysis plots

--- SamplingExperiment/plot_test.py
+++ SamplingExperiment/plot_test.py
@@ -8,7 +8,6 @@ import os
 from scipy import stats
 os.chdir(r"C:\Users\andersgm\Documents\Courses\02466 Project work F21\Project")
 label_dict = {'chew': 0, 'elpp': 1, 'eyem': 2, 'musc': 3, 'shiv': 4, 'null': 5}
-color_dict = {0: "yellow", 1: "red", 2: "lime", 3: "orange", 4: "purple", 5: "cornflowerblue"}
 
 #%%
 
@@ -24,71 +23,61 @@ ypred = stats.mode(ypreds, axis=0).mode[0]
 #%%
 def clean(a):
     a = deepcopy(a)
-    for i in range(2, len(a)-2):
-        if a[i-2] == a[i-1] and a[i-1] == a[i+1] and a[i+1] == a[i+2]:
-            a[i] = a[i-1]
-        
-    for i in range(3, len(a)-4):
-        if a[i-3] == a[i-2] and a[i-2] == a[i-1] and a[i-1] == a[i+2] and a[i+2] == a[i+3] and a[i+3] == a[i+4]:
-            a[i] = a[i-1]
-            a[i+1] = a[i-1]
-            
-    mask = np.zeros(len(a))
-    for i in range(len(a)-4):
-        if np.all(a[i:i+4] == a[i]):
-            mask[i:i+4] = 1
-            
-    for i in range(len(a)):
-        if not mask[i]:
-            a[i] = 5
     
-    # mask = np.zeros(len(a))
-    # for i in range(len(a)-5):
-    #     if np.all(a[i:i+5] == a[i]):
-    #         mask[i:i+5] = 1
+    for n in range(1, 2):
+        for i in range(n+1, len(a)-n*2-1):
+            if np.all(a[i-n-1:i] == a[i-n-1]) and np.all(a[i-n-1] == a[i+n:i+2*n+1]):
+                a[i:i+n] = a[i-n-1]
             
-    # for i in range(len(a)):
-    #     if not mask[i]:
-    #         a[i] = 5
-    
-    # a = deepcopy(a)
-    # mask = np.zeros(len(a)) 
-    # for i in range(len(a)-6):
-    #     if np.all(a[i:i+6] == a[i]):
-    #         mask[i:i+6] = 1
-            
-    # for i in range(len(a)):
-    #     if not mask[i]:
-    #         a[i] = 5
-    
+    for n in range(4, 7):
+        mask = np.zeros(len(a))
+        for i in range(len(a)-n):
+            if np.all(a[i:i+n] == a[i]):
+                mask[i:i+n] = True
+                
+        for i in range(len(a)):
+            if not mask[i]:
+                a[i] = 5
+                
+    for n in range(4, 7):
+        mask = np.zeros(len(a))
+        for i in range(len(a)-n):
+            if np.all(a[i:i+n] == a[i]):
+                mask[i:i+n] = True
+                
+        for i in range(len(a)):
+            if not mask[i]:
+                a[i] = 5   
+                
     return a
 
 def clean2(a):
     a = deepcopy(a)
-    for i in range(2, len(a)-2):
-        if a[i-2] == a[i-1] and a[i-1] == a[i+1] and a[i+1] == a[i+2]:
-            a[i] = a[i-1]
+    for _ in range(10):
+        for n in range(2, 5):
+            for i in range(n+1, len(a)-n*2-1):
+                if np.all(a[i-n-1:i] == a[i-n-1]) and np.all(a[i-n-1] == a[i+n:i+2*n+1]):
+                    a[i:i+n] = a[i-n-1]
         
-    for i in range(3, len(a)-4):
-        if a[i-3] == a[i-2] and a[i-2] == a[i-1] and a[i-1] == a[i+2] and a[i+2] == a[i+3] and a[i+3] == a[i+4]:
-            a[i] = a[i-1]
-            a[i+1] = a[i-1]
-            
-    mask = np.zeros(len(a))
-    for i in range(len(a)-4):
-        if np.all(a[i:i+4] == a[i]):
-            mask[i:i+4] = 1
-            
-    for i in range(len(a)):
-        if not mask[i]:
-            a[i] = 5
-     
-    for n in range(1, 8):
-        for i in range(n, len(a)-n*2):
-            if np.all(a[i-n:i] == 5) and np.all(a[i+n:i+n*2] == 5) and np.all(a[i:i+n] == 1):
-                a[i:i+2] = 5
+    for _ in range(10):
+        for n in range(4, 20):
+            mask = np.zeros(len(a))
+            for i in range(len(a)-n):
+                if np.all(a[i:i+n] == a[i]):
+                    mask[i:i+n] = True
+                    
+            for i in range(len(a)):
+                if not mask[i]:
+                    a[i] = 5
     
+    for _ in range(10):
+        for n in range(2, 9):
+            for i in range(n+1, len(a)-n*2-1):
+                if np.all(a[i-n-1:i] == a[i-n-1]) and np.all(a[i-n-1] == a[i+n:i+2*n+1]):
+                    a[i:i+n] = a[i-n-1]
+            
     return a
+
 
 
 def plot_artifacts(ax, y, dataframe):
@@ -100,18 +89,22 @@ def plot_artifacts(ax, y, dataframe):
     
     ax.tick_params(left =False)
     
-    
 
 #%%
+color_dict = {0: "#f5cf40", 1: "#e63f47", 2: "#0ed280", 3: "#fc7323", 4: "#79218f", 5: "#828bf2"} 
 
 patient_name = np.random.choice( np.unique(df["Name"]))
-patient_name = "00000254_s005_t000"
+#patient_name = "00000254_s005_t000"
+#patient_name = "00008181_s003_t001"
 #patient_name = "00009362_s001_t001"
+#patient_name = "00007823_s001_t001"
+#patient_name = "00004473_s002_t001"
+patient_name = "00010212_s001_t000"
 patient = deepcopy(df[df["Name"] == patient_name])
 patient = patient.sort_values(by="Start", ascending=True, key=lambda col: col.values).reset_index(drop=True)
 prediction = ypred[patient["index"].values]
 
-fig, ax = plt.subplots(nrows=4, ncols=1, sharex=True)
+fig, ax = plt.subplots(nrows=3, ncols=1, sharex=True)
 
 ax[0].plot([0, 0],[0, 1], color='white')
 plot_artifacts(ax[0], patient["Multiclass label"], patient)
@@ -125,12 +118,12 @@ ax[2].plot([0, 0],[0, 1], color='white')
 plot_artifacts(ax[2], clean(prediction), patient)
 ax[2].set_ylabel("Simple Filter")
 
-ax[3].plot([0, 0],[0, 1], color='white')
-plot_artifacts(ax[3], clean2(clean(prediction)), patient)
-ax[3].set_ylabel("Advanced Filter")
+# ax[3].plot([0, 0],[0, 1], color='white')
+# plot_artifacts(ax[3], clean2(prediction), patient)
+# ax[3].set_ylabel("Advanced Filter")
 
 
 
 plt.tight_layout()
 plt.show()
-#%%
+     #%%
--- SamplingExperiment/usecase_filter.py
+++ SamplingExperiment/usecase_filter.py
@@ -9,7 +9,6 @@ from scipy import stats
 from sklearn.metrics import classification_report
 os.chdir(r"C:\Users\andersgm\Documents\Courses\02466 Project work F21\Project")
 label_dict = {'chew': 0, 'elpp': 1, 'eyem': 2, 'musc': 3, 'shiv': 4, 'null': 5}
-color_dict = {0: "yellow", 1: "red", 2: "lime", 3: "orange", 4: "purple", 5: "cornflowerblue"}
 
 #%%
 
@@ -18,7 +17,27 @@ df["index"] = df.index
 
 ypreds = np.load("predictions_LDA_4_5_5_5_55784899_04-06-21_20-08-15.npy")
 
+#%%
 def clean(a):
+    a = deepcopy(a)
+    
+    for n in range(1, 2):
+        for i in range(n+1, len(a)-n*2-1):
+            if np.all(a[i-n-1:i] == a[i-n-1]) and np.all(a[i-n-1] == a[i+n:i+2*n+1]):
+                a[i:i+n] = a[i-n-1]
+            
+    for n in range(4, 7):
+        mask = np.zeros(len(a))
+        for i in range(len(a)-n):
+            if np.all(a[i:i+n] == a[i]):
+                mask[i:i+n] = True
+                
+        for i in range(len(a)):
+            if not mask[i]:
+                a[i] = 5    
+    return a
+
+def clean2(a):
     a = deepcopy(a)
     for i in range(2, len(a)-2):
         if a[i-2] == a[i-1] and a[i-1] == a[i+1] and a[i+1] == a[i+2]:
@@ -38,26 +57,40 @@ def clean(a):
         if not mask[i]:
             a[i] = 5
     
-    # mask = np.zeros(len(a))
-    # for i in range(len(a)-5):
-    #     if np.all(a[i:i+5] == a[i]):
-    #         mask[i:i+5] = 1
+    mask = np.zeros(len(a))
+    for i in range(len(a)-5):
+        if np.all(a[i:i+5] == a[i]):
+            mask[i:i+5] = 1
             
-    # for i in range(len(a)):
-    #     if not mask[i]:
-    #         a[i] = 5
-    
-    # a = deepcopy(a)
-    # mask = np.zeros(len(a)) 
-    # for i in range(len(a)-6):
-    #     if np.all(a[i:i+6] == a[i]):
-    #         mask[i:i+6] = 1
+    for i in range(len(a)):
+        if not mask[i]:
+            a[i] = 5
             
-    # for i in range(len(a)):
-    #     if not mask[i]:
-    #         a[i] = 5
+    mask = np.zeros(len(a)) 
+    for i in range(len(a)-6):
+        if np.all(a[i:i+6] == a[i]):
+            mask[i:i+6] = 1
+            
+    for i in range(len(a)):
+        if not mask[i]:
+            a[i] = 5
     
+    mask = np.zeros(len(a)) 
+    for i in range(len(a)-7):
+        if np.all(a[i:i+7] == a[i]):
+            mask[i:i+7] = 1
+            
+    for i in range(len(a)):
+        if not mask[i]:
+            a[i] = 5
+            
+            
+    for i in range(4, len(a)-6):
+        if np.all(a[i-7:i] == a[i-7]) and np.all(a[i-7] == a[i+6:i+6+7]):
+            a[i:i+6] = a[i-7]
+            
     return a
+
 
 #%%
     
@@ -69,13 +102,25 @@ for patient_name in tqdm(np.unique(df["Name"])):
     
     for i in range(5):
         filtered_ypreds[i][patient["index"].values] = clean(ypreds[i][patient["index"].values])
-    
-    
-#%%
 
-print(classification_report(np.tile(df["Multiclass label"].values, filtered_ypreds.shape[0]), filtered_ypreds.flatten(), target_names=list(label_dict.keys())))
-print(["{:.3f}".format(np.mean(filtered_ypreds.flatten()[np.tile(df["Multiclass label"].values, filtered_ypreds.shape[0]) == i] == i)) for i in range(6)])
+filtered_ypreds2 = np.empty(ypreds.shape)
+filtered_ypreds2.fill(-1)
+
+for patient_name in tqdm(np.unique(df["Name"])):
+    patient = df[df["Name"] == patient_name].sort_values(by="Start", ascending=True, key=lambda col: col.values).reset_index(drop=True)
+    
+    for i in range(5):
+        filtered_ypreds2[i][patient["index"].values] = clean2(ypreds[i][patient["index"].values])
+    
 
 #%%
 print(classification_report(np.tile(df["Multiclass label"].values, ypreds.shape[0]), ypreds.flatten(), target_names=list(label_dict.keys())))
 print(["{:.3f}".format(np.mean(ypreds.flatten()[np.tile(df["Multiclass label"].values, ypreds.shape[0]) == i] == i)) for i in range(6)])
+
+#%%
+print(classification_report(np.tile(df["Multiclass label"].values, filtered_ypreds.shape[0]), filtered_ypreds.flatten(), target_names=list(label_dict.keys())))
+print(["{:.3f}".format(np.mean(filtered_ypreds.flatten()[np.tile(df["Multiclass label"].values, filtered_ypreds.shape[0]) == i] == i)) for i in range(6)])
+
+#%%
+print(classification_report(np.tile(df["Multiclass label"].values, filtered_ypreds2.shape[0]), filtered_ypreds.flatten(), target_names=list(label_dict.keys())))
+print(["{:.3f}".format(np.mean(filtered_ypreds.flatten()[np.tile(df["Multiclass label"].values, filtered_ypreds.shape[0]) == i] == i)) for i in range(6)])
